e308d955668c68717f9e7c81ddecd67b480e3160
kenyk7 <kenykalin@gmail.com>
2024-03-16 17:17:53 -0500

    feat: update all dependencies, - jest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+// import eslintPlugin from 'vite-plugin-eslint';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import eslint from 'vite-plugin-eslint';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), tsconfigPaths()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      eslintOptions: {
+        cache: false,
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
